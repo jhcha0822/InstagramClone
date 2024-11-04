5377a71bf32d4218e265b2055ad46b6cdbe9779f
[SeongJin Yun] <[ckwdktek12@gmail.com]>
2024-11-04 22:12:18 +0900

    no message

--- BE/build/generated/sources/annotationProcessor/java/main/com/project/instagramclone/entity/member/QMemberEntity.java
+++ BE/build/generated/sources/annotationProcessor/java/main/com/project/instagramclone/entity/member/QMemberEntity.java
@@ -19,7 +19,27 @@ public class QMemberEntity extends EntityPathBase<MemberEntity> {
 
     public static final QMemberEntity memberEntity = new QMemberEntity("memberEntity");
 
+    public final BooleanPath activated = createBoolean("activated");
+
+    public final StringPath bio = createString("bio");
+
+    public final StringPath email = createString("email");
+
     public final NumberPath<Long> memberId = createNumber("memberId", Long.class);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath oauth2Provider = createString("oauth2Provider");
+
+    public final StringPath oauthId = createString("oauthId");
+
+    public final StringPath password = createString("password");
+
+    public final StringPath profilePic = createString("profilePic");
+
+    public final StringPath role = createString("role");
+
+    public final StringPath username = createString("username");
 
     public QMemberEntity(String variable) {
         super(MemberEntity.class, forVariable(variable));

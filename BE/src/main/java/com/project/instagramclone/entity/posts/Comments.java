63ea98d7f28c5419d5eb55983e41fd9abd83ad20
[SeongJin Yun] <[ckwdktek12@gmail.com]>
2024-11-15 15:43:00 +0900

    댓글/대댓글 프론트 조회 추가

--- BE/src/main/java/com/project/instagramclone/entity/posts/Comments.java
+++ BE/src/main/java/com/project/instagramclone/entity/posts/Comments.java
@@ -1,5 +1,8 @@
 package com.project.instagramclone.entity.posts;
 
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,8 +23,9 @@ public class Comments {
 
     private String postId;
     private String parentCommentId; // 대댓글의 경우 부모 댓글의 ID, 없으면 null
-
-    private List<String> children;  // 자식 댓글(대댓글)의 ID 리스트
-
     private long regdate;
+
+    @OneToMany(fetch = FetchType.LAZY)  // 자식 댓글은 지연 로딩으로 처리 (필요시 즉시 로딩 가능)
+    @JoinColumn(name = "parentCommentId", referencedColumnName = "id", insertable = false, updatable = false)  // 부모 댓글과 연결
+    private List<Comments> children;  // 부모 댓글의 자식 댓글들
 }
